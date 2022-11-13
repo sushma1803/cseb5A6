136c23ea8a9b7a58869da9d08433ffd597e47712
Lenovo <Lenovo@DESKTOP-2CUCI5T>
2022-11-13 21:43:03 +0530

    commit maven

---
+++ src/main/java/csebmaven/cseb5A6/App.java
@@ -0,0 +1,13 @@
+package csebmaven.cseb5A6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/csebmaven/cseb5A6/AppTest.java
@@ -0,0 +1,20 @@
+package csebmaven.cseb5A6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
